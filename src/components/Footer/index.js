4259e50b20d6425780daffc1b615149c484ba5e5
Arthur Vera <arthur@mcube.fr>
2023-06-22 16:55:46 +0200

    Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,21 @@
 import './style.scss'
-// import instagram from '../../assets/svg/instagram.svg'
-// import facebook from '../../assets/svg/facebook.svg'
-// import youtube from '../../assets/svg/youtube.svg'
+import {faFacebook, faInstagram, faYoutube} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Link} from "react-router-dom";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+
 
 const Footer = () => {
     return (
         <footer>
             <div className="top-part">
-                <div className="get-in-touch">
-                    <h2>Restons <br/> en contact</h2>
-                </div>
+                <a href={"/contact"} className="get-in-touch">
+                    <h2>Restons <FontAwesomeIcon icon={faEnvelope} /> <br/> en contact</h2>
+                </a>
                 <ul className="footer-menu">
-                   <li>
-                       <a href="/presentation">Présentation</a>
-                   </li>
+                    <li>
+                        <a href="/presentation">Présentation</a>
+                    </li>
                     <li>
                         <a href="/medias">Médias</a>
                     </li>
@@ -29,13 +31,15 @@ const Footer = () => {
                 </ul>
             </div>
             <div className="socials-part">
-                <div className="social instagram">
-                    {/*<img src="" alt=""/>*/}
-                </div>
-                <div className="social facebook">
-                </div>
-                <div className="social youtube">
-                </div>
+                <a href={"https://www.instagram.com/jazzenvelay/?hl=fr"} className="social instagram">
+                    <FontAwesomeIcon icon={faInstagram}/>
+                </a>
+                <a href="https://www.facebook.com/jazzenvelay43/?locale=fr_FR" className="social facebook">
+                    <FontAwesomeIcon icon={faFacebook}/>
+                </a>
+                <a href="https://www.youtube.com/channel/UCl71xr9U8gRhTw8FAo6rg8A" className="social youtube">
+                    <FontAwesomeIcon icon={faYoutube}/>
+                </a>
             </div>
             <div className="bottom-part">
                 <div className="copyrights">
